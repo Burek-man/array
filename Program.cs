c57fb2efdbdbb6f5a1e79a865dd83fa174183229
Burek-man <Alnu07001@utb.vaxjo.se>
2024-10-23 10:57:02 +0200

    array

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers= new int[5];
+
+for (int i =0; i <numbers.Length; i++);
+
+{
+    Console.WriteLine("skriv ett tal");
+    numbers[i] = int.Parse(Console.ReadLine());
+
+}
+
+Console.WriteLine
